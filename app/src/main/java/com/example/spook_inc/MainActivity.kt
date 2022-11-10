6c586f1f5ed6465fd7b659c4fb25c3f42a33656c
Tan Dorian <dorian.tan@he-arc.ch>
2022-11-10 14:36:34 +0100

    button, background and title

--- app/src/main/java/com/example/spook_inc/MainActivity.kt
+++ app/src/main/java/com/example/spook_inc/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.spook_inc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCatchGhosts = findViewById<ImageButton>(R.id.imgBtnCatchGhosts)
+
+    }
+    fun catchGhosts(){
+
     }
 }
