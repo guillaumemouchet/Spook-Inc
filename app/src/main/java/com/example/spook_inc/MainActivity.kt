79b0d0e218e7d84d8d7b8a3eedf11bdd22cd5c01
Tan Dorian <dorian.tan@he-arc.ch>
2022-11-17 13:22:40 +0100

    flashLightOff/On

--- app/src/main/java/com/example/spook_inc/MainActivity.kt
+++ app/src/main/java/com/example/spook_inc/MainActivity.kt
@@ -4,16 +4,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnCatchGhosts = findViewById<ImageButton>(R.id.imgBtnCatchGhosts)
+        val btnGhost = findViewById<ImageButton>(R.id.imgBtnGhost)
+
+        val flashLightOff = findViewById<ImageButton>(R.id.imgBtnCatchGhostsOff)
+        //val flashLightOn = findViewById<ImageButton>(R.id.imgBtnCatchGhostsOff)
+
+        val btnTraining = findViewById<Button>(R.id.btnTraining)
+        val btnSpook = findViewById<Button>(R.id.btnSpook)
+
+        var lightOff = true
+
+        flashLightOff.setOnClickListener(){
+            if(lightOff){
+                flashLightOff.setImageResource(R.drawable.flashlight_on);
+                btnGhost.isVisible = true;
+                lightOff = false;
+            }else{
+                flashLightOff.setImageResource(R.drawable.flashlight_off);
+                btnGhost.isVisible = false;
+                lightOff = true;
+            }
+
+
+        }
+
+
 
     }
     fun catchGhosts(){
 
     }
+
 }
