9235c75f847c4ea97a1e9306718e1a3c4773a2d5
Tan Dorian <dorian.tan@he-arc.ch>
2022-11-17 14:02:15 +0100

    add intent and activities

---
+++ app/src/main/java/com/example/spook_inc/SpookActivity.kt
@@ -0,0 +1,11 @@
+package com.example.spook_inc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spook)
+    }
+}
