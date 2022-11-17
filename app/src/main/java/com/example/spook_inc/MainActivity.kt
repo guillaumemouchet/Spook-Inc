095325a49a965a57ea9e50d13687595762639e10
Tan Dorian <dorian.tan@he-arc.ch>
2022-11-17 13:53:48 +0100

    foreground

--- app/src/main/java/com/example/spook_inc/MainActivity.kt
+++ app/src/main/java/com/example/spook_inc/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
@@ -19,16 +20,24 @@ class MainActivity : AppCompatActivity() {
         val btnTraining = findViewById<Button>(R.id.btnTraining)
         val btnSpook = findViewById<Button>(R.id.btnSpook)
 
+        var imgForeground = findViewById<ImageView>(R.id.imgForeground)
+
         var lightOff = true
 
         flashLightOff.setOnClickListener(){
             if(lightOff){
                 flashLightOff.setImageResource(R.drawable.flashlight_on);
                 btnGhost.isVisible = true;
+                btnTraining.isVisible = true;
+                btnSpook.isVisible = true;
+                imgForeground.alpha = 0.0f;
                 lightOff = false;
             }else{
                 flashLightOff.setImageResource(R.drawable.flashlight_off);
                 btnGhost.isVisible = false;
+                btnTraining.isVisible = false;
+                btnSpook.isVisible = false;
+                imgForeground.alpha = 0.8f;
                 lightOff = true;
             }
 
