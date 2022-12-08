4fffbd4fa76f54e13eeb0546901537e1a75d3b75
Tan Dorian <dorian.tan@he-arc.ch>
2022-12-08 14:02:22 +0100

    test with class Ghost

--- app/src/main/java/com/example/spook_inc/TrainingActivity.kt
+++ app/src/main/java/com/example/spook_inc/TrainingActivity.kt
@@ -2,10 +2,25 @@ package com.example.spook_inc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+
+import android.widget.Toast
 
 class TrainingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
+
+        val btnGhost = findViewById<ImageButton>(R.id.image6)
+
+        btnGhost.setOnClickListener(){
+            Toast.makeText(this,"Hello charlie",Toast.LENGTH_SHORT).show()
+        }
     }
+
+}
+
+class Ghost(name: String)
+{
+    val name = name
 }
