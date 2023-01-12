f456bf1fb381b25498d82f8dfff2840fd82343ba
Tan Dorian <dorian.tan@he-arc.ch>
2023-01-12 13:15:53 +0100

    class Ghost

--- app/src/main/java/com/example/spook_inc/TrainingActivity.kt
+++ app/src/main/java/com/example/spook_inc/TrainingActivity.kt
@@ -2,6 +2,7 @@ package com.example.spook_inc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
 
 import android.widget.Toast
@@ -12,24 +13,28 @@ class TrainingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_training)
 
         val btnGhost = findViewById<ImageButton>(R.id.image6)
+        val ghost = Ghost(1,"Charlie",10);
 
         btnGhost.setOnClickListener(){
-            Toast.makeText(this,"Hello charlie",Toast.LENGTH_SHORT).show()
-            println("Coucou")
+            //Toast.makeText(this,"Hello Charlie",Toast.LENGTH_SHORT).show()
+            //println("Coucou")
+            ghost.main()
         }
+
+
     }
 
 }
 
-class Ghost()
+class Ghost(val id: Int, val name: String, val strength: Int)
 {
-    //val id
-    // val name
-    //val strength
+    //val id: Int,
+    //val name: String,
+    //val strength: Int,
     //val img
 
     fun main()
     {
-
+        Log.i("Ghost", name)
     }
 }
