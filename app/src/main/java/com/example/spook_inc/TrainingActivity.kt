8da6ce57e42136df7082a4c6f046d62d837a966f
Tan Dorian <dorian.tan@he-arc.ch>
2023-01-19 13:04:14 +0100

    btnAddTeam

--- app/src/main/java/com/example/spook_inc/TrainingActivity.kt
+++ app/src/main/java/com/example/spook_inc/TrainingActivity.kt
@@ -3,22 +3,32 @@ package com.example.spook_inc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 
 import android.widget.Toast
+import androidx.core.view.isInvisible
 
 class TrainingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_training)
 
+        val btnAddTeam = findViewById<Button>(R.id.btnAdd)
+
         val btnGhost = findViewById<ImageButton>(R.id.image6)
-        val ghost = Ghost(1,"Charlie",10);
+        val ghost = Ghost(1,"Charlie",10)
 
         btnGhost.setOnClickListener(){
+            btnAddTeam.visibility = View.VISIBLE
             ghost.main()
+            btnAddTeam.text = "Add ${ghost.name}"
         }
 
+        btnAddTeam.setOnClickListener(){
+            Toast.makeText(this, "AddTeam: ${ghost.name}", Toast.LENGTH_SHORT).show()
+        }
 
     }
 
