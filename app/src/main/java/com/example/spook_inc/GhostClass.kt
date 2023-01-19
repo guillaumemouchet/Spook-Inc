385b3ce97ccad34c69a16489f833a62cef92d405
Tan Dorian <dorian.tan@he-arc.ch>
2023-01-19 13:11:05 +0100

    separate GhostClass and TrainingActivity

---
+++ app/src/main/java/com/example/spook_inc/GhostClass.kt
@@ -0,0 +1,21 @@
+package com.example.spook_inc
+
+import android.util.Log
+
+class Ghost(val id: Int, val name: String, val strength: Int)
+{
+    //val id: Int,
+    //val name: String,
+    //val strength: Int,
+    //val img
+
+    fun main()
+    {
+        Log.i("Ghost", name)
+    }
+
+    fun display()
+    {
+
+    }
+}
