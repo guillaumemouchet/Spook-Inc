95958924adc993ff25b8509b2f1179e60d660095
guillaumemouchet <guillaume.mouchet01@outlook.com>
2023-05-04 11:23:14 +0200

    added deleted file

--- app/src/main/java/com/example/spook_inc/activitiy/MainActivity.kt
+++ app/src/main/java/com/example/spook_inc/activitiy/MainActivity.kt
@@ -68,6 +68,12 @@ class MainActivity : AppCompatActivity() {
         if(!ghostFile.exists()) {
             ghostFile.createNewFile()
         }
+        /* DEBUG
+        else{
+            ghostFile.delete()
+            ghostFile.createNewFile()
+
+        }*/
 
         // Create Team
         val filenameTeam = "my_team.json"
@@ -76,137 +82,143 @@ class MainActivity : AppCompatActivity() {
         {
             ghostFileTeam.createNewFile()
         }
+/* DEBUG
+else
+{
+   ghostFileTeam.delete()
+   ghostFileTeam.createNewFile()
+}*/
 
-        /*
-         * The Flashlight hides some elements when turned off
-         * The player must click on it to illuminate the activity and see the buttons
-         * A click sound is made on each button press
-         */
-        flashLightOff.setOnClickListener() {
+/*
+* The Flashlight hides some elements when turned off
+* The player must click on it to illuminate the activity and see the buttons
+* A click sound is made on each button press
+*/
+flashLightOff.setOnClickListener() {
 
-            var lightclick = MediaPlayer.create(this, R.raw.flashlight);
-            lightclick.setVolume(1f, 1f)
-            lightclick.start();
+   var lightclick = MediaPlayer.create(this, R.raw.flashlight);
+   lightclick.setVolume(1f, 1f)
+   lightclick.start();
 
-            if (lightOff) {
+   if (lightOff) {
 
-                flashLightOff.setImageResource(R.drawable.flashlight_on);
-                btnGhost.isVisible = true;
-                btnTraining.isVisible = true;
-                btnSpook.isVisible = true;
-                imgForeground.alpha = 0.0f;
-                lightOff = false;
+       flashLightOff.setImageResource(R.drawable.flashlight_on);
+       btnGhost.isVisible = true;
+       btnTraining.isVisible = true;
+       btnSpook.isVisible = true;
+       imgForeground.alpha = 0.0f;
+       lightOff = false;
 
-            } else {
+   } else {
 
-                flashLightOff.setImageResource(R.drawable.flashlight_off);
-                btnGhost.isVisible = false;
-                btnTraining.isVisible = false;
-                btnSpook.isVisible = false;
-                imgForeground.alpha = 0.8f;
-                lightOff = true;
-            }
+       flashLightOff.setImageResource(R.drawable.flashlight_off);
+       btnGhost.isVisible = false;
+       btnTraining.isVisible = false;
+       btnSpook.isVisible = false;
+       imgForeground.alpha = 0.8f;
+       lightOff = true;
+   }
 
-        }
+}
 
-        /*
-         * Add the Listener on the differents Buttons and put music on them
-         * The set Volume doesn't seems to change much
-         */
+/*
+* Add the Listener on the differents Buttons and put music on them
+* The set Volume doesn't seems to change much
+*/
 
-        btnGhost.setOnClickListener() {
-            var ghostnoise = MediaPlayer.create(this, R.raw.ghost);
-            ghostnoise.setVolume(0.9f, 0.9f)
-            ghostnoise.start();
-            catchGhosts();
-        }
+btnGhost.setOnClickListener() {
+   var ghostnoise = MediaPlayer.create(this, R.raw.ghost);
+   ghostnoise.setVolume(0.9f, 0.9f)
+   ghostnoise.start();
+   catchGhosts();
+}
 
-        btnTraining.setOnClickListener() {
-            var whistlenoise = MediaPlayer.create(this, R.raw.whistle);
-            whistlenoise.setVolume(0.05f, 0.05f)
-            whistlenoise.start();
-            training();
-        }
+btnTraining.setOnClickListener() {
+   var whistlenoise = MediaPlayer.create(this, R.raw.whistle);
+   whistlenoise.setVolume(0.05f, 0.05f)
+   whistlenoise.start();
+   training();
+}
 
-        btnSpook.setOnClickListener() {
-            var kidnoise = MediaPlayer.create(this, R.raw.cri2);
-            kidnoise.setVolume(0.08f, 0.08f)
-            kidnoise.start();
-            spook();
-        }
+btnSpook.setOnClickListener() {
+   var kidnoise = MediaPlayer.create(this, R.raw.cri2);
+   kidnoise.setVolume(0.08f, 0.08f)
+   kidnoise.start();
+   spook();
+}
 
-    }
+}
 
-    /*
-     * Open the differents Activities
-     */
+/*
+* Open the differents Activities
+*/
 
-    private fun catchGhosts() {
-        val intent = Intent(this, CameraActivity::class.java);
-        startActivity(intent);
-    }
+private fun catchGhosts() {
+val intent = Intent(this, CameraActivity::class.java);
+startActivity(intent);
+}
 
-    private fun training() {
-        val intent = Intent(this, TrainingActivity::class.java);
-        startActivity(intent);
-    }
+private fun training() {
+val intent = Intent(this, TrainingActivity::class.java);
+startActivity(intent);
+}
 
-    private fun spook() {
-        val intent = Intent(this, SpookActivity::class.java);
-        startActivity(intent);
-    }
+private fun spook() {
+val intent = Intent(this, SpookActivity::class.java);
+startActivity(intent);
+}
 
 
-    //Cycle de vie d'une application
+//Cycle de vie d'une application
 
-    override fun onStart() {
-        //Start sound of BG
-        startService(Intent(this, BackgroundSoundService::class.java))
-        super.onStart()
-    }
+override fun onStart() {
+//Start sound of BG
+startService(Intent(this, BackgroundSoundService::class.java))
+super.onStart()
+}
 
-    override fun onRestart() {
-        // "super" after (continues flow)
-        super.onRestart()
-    }
+override fun onRestart() {
+// "super" after (continues flow)
+super.onRestart()
+}
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        // "super" before (populates our Bundle object)
-        super.onRestoreInstanceState(savedInstanceState)
+override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+// "super" before (populates our Bundle object)
+super.onRestoreInstanceState(savedInstanceState)
 
-        // Here, the Bundle object is never null
-        counter = savedInstanceState.getInt(COUNTER_KEY, 0)
-    }
+// Here, the Bundle object is never null
+counter = savedInstanceState.getInt(COUNTER_KEY, 0)
+}
 
-    override fun onResume() {
+override fun onResume() {
 
-        // "super" after (continues flow)
-        super.onResume()
-    }
+// "super" after (continues flow)
+super.onResume()
+}
 
-    override fun onPause() {
-        startService(Intent(this, BackgroundSoundService::class.java))
-        super.onPause()
-    }
+override fun onPause() {
+startService(Intent(this, BackgroundSoundService::class.java))
+super.onPause()
+}
 
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        ++counter
-        savedInstanceState.putInt(COUNTER_KEY, counter)
+override fun onSaveInstanceState(savedInstanceState: Bundle) {
+++counter
+savedInstanceState.putInt(COUNTER_KEY, counter)
 
-        // "super" after (saves our Bundle object, continues flow)
-        super.onSaveInstanceState(savedInstanceState)
-    }
+// "super" after (saves our Bundle object, continues flow)
+super.onSaveInstanceState(savedInstanceState)
+}
 
-    override fun onStop() {
-        // "super" after (continues flow)
+override fun onStop() {
+// "super" after (continues flow)
 
-        super.onStop()
-    }
+super.onStop()
+}
 
-    override fun onDestroy() {
-        stopService(Intent(this, BackgroundSoundService::class.java))
+override fun onDestroy() {
+stopService(Intent(this, BackgroundSoundService::class.java))
 
-        // "super" after (continues flow)
-        super.onDestroy()
-    }
+// "super" after (continues flow)
+super.onDestroy()
+}
 }
